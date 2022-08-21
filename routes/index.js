57d4239bb59f8a7ff967891ded71cd80660741e0
Joshtri <stuffofyos1516@gmail.com>
2022-08-21 16:55:17 +0800

    Newest

---
+++ routes/index.js
@@ -0,0 +1,69 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/main-admin', function(req, res, next) {
+  res.render('main-admin');
+});
+
+// GET data-keluarga
+router.get('/data-keluarga', function(req, res, next) {
+  res.render('data-keluarga');
+});
+
+// GET data-penduduk
+router.get('/data-penduduk', function(req, res, next) {
+  res.render('data-penduduk');
+});
+
+// GET data-kelahiran
+router.get('/data-kelahiran', function(req, res, next) {
+  res.render('data-kelahiran');
+});
+// GET data-kematian
+router.get('/data-kematian', function(req, res, next) {
+  res.render('data-kematian');
+});
+// GET data-pekerjaan
+router.get('/data-pekerjaan', function(req, res, next) {
+  res.render('data-pekerjaan');
+});
+// GET data-pindah
+router.get('/data-pindah', function(req, res, next) {
+  res.render('data-pindah');
+});
+// GET data-masuk
+router.get('/data-masuk', function(req, res, next) {
+  res.render('data-masuk');
+});
+// GET data-penduduk
+router.get('/data-penduduk', function(req, res, next) {
+  res.render('data-penduduk');
+});
+
+// GET data-kbli
+router.get('/data-kbli', function(req, res, next) {
+  res.render('data-kbli');
+});
+
+// GET data-kbli
+router.get('/data-umkm', function(req, res, next) {
+  res.render('data-umkm');
+});
+
+
+
+
+
+
+
+// GET login view
+router.get('/login', function(req, res, next) {
+  res.render('login');
+});
+
+
+
+
+
+module.exports = router;
