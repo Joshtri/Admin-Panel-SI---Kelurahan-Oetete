a79146a6e907ee4f760f478dfd608d875bd1c615
Joshtri <stuffofyos1516@gmail.com>
2022-08-23 14:55:24 +0800

    fck

---
+++ controllers/index.js
@@ -0,0 +1,5 @@
+const keluarga= require('./keluarga');
+const controller = {};
+
+controller.keluarga = keluarga;
+module.exports = controller;
--- controllers/keluarga.js
+++ controllers/keluarga.js
@@ -2,5 +2,28 @@ const model = require('../config/model/index');
 const controller = {};
 
 controller.getAll = async function(req,res){
-    
+    try{
+        await model.keluarga.findAll()
+        .then((result) =>{
+            if(result.length > 0){
+                res.status(200).json({
+                    message: 'Get The Method Mahasiswa',
+                    data: result
+                });
+            }
+            else{
+                res.status(200).json({
+                    message: 'Tidak ada Data',
+                    data:[]
+                });
+            }
+        })
+    }catch(error){
+        res.status(404).json({
+            message: error
+
+        });
+    }
 }
+
+module.exports = controller;
