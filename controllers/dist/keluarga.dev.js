14dba4f844a000a218cb8c146b2f78e6aa302fea
Joshtri <stuffofyos1516@gmail.com>
2022-08-23 11:07:19 +0800

    fuck yea

---
+++ controllers/dist/keluarga.dev.js
@@ -0,0 +1,17 @@
+"use strict";
+
+var model = require('../config/model/index');
+
+var controller = {};
+
+controller.getAll = function _callee(req, res) {
+  return regeneratorRuntime.async(function _callee$(_context) {
+    while (1) {
+      switch (_context.prev = _context.next) {
+        case 0:
+        case "end":
+          return _context.stop();
+      }
+    }
+  });
+};
---
+++ controllers/keluarga.js
@@ -0,0 +1,6 @@
+const model = require('../config/model/index');
+const controller = {};
+
+controller.getAll = async function(req,res){
+    
+}
